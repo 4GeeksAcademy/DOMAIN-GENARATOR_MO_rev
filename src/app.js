03f5979ac00c46fb71ad61f88fce82bd0e53257a
MarOlivieri <141793518+MarOlivieri@users.noreply.github.com>
2024-05-20 12:24:01 +0000

    Actualizacion formato proyecto

--- src/app.js
+++ src/app.js
@@ -9,3 +9,17 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+let pronoun = ["the", "our"];
+let adj = ["great", "big"];
+let noun = ["jogger", "racoon"];
+let extension = [".us", ".com", ".net", ".io"];
+
+for (let mix of pronoun) {
+  for (let value of adj) {
+    for (let num of noun) {
+      for (let digit of extension) {
+        console.log(mix + value + num + digit);
+      }
+    }
+  }
+}
